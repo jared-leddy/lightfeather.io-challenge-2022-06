cb9465e2d6a72fdbdbef8077a339a2a3baab068b
Jared Ledbetter <jared.ledbetter@outlook.com>
2022-06-30 21:21:44 -0400

    Create backend.module.ts

---
+++ src/backend/backend.module.ts
@@ -0,0 +1,16 @@
+// NPM Modules
+import { Module } from '@nestjs/common';
+
+// Custom Modules
+import { BackendController } from './backend.controller';
+import { BackendService } from './backend.service';
+
+// create module decorator
+// create class auth module
+@Module({
+    imports: [],
+    exports: [],
+    controllers: [BackendController],
+    providers: [BackendService]
+})
+export class AccountModule {}
