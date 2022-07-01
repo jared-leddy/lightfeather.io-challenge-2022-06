79aaaafac89bf21092f9d3564eda7af7944d7725
Jared Ledbetter <jared.ledbetter@outlook.com>
2022-07-01 12:48:45 -0400

    Update backend.js

--- public/js/backend.js
+++ public/js/backend.js
@@ -1,60 +1,34 @@
-export const login = (email, password) => {
-    console.log('Begin capture email and password.');
-    $.ajax({
-        method: 'post',
-        url: '/api/v1/account/login/',
-        data: {
-            email,
-            password
-        }
+export const getSupervisors = () => {
+    console.log('getSupervisors: Begin');
+    return $.ajax({
+        method: 'get',
+        url: '/api/supervisors/'
     })
-        .done(function () {
-            console.log('POST: /api/v1/account/login/ | status: success');
-            setTimeout(() => {
-                window.location.assign('/');
-            }, 3000);
+        .done(function (response) {
+            console.log('GET: /api/supervisors/ | status: success');
+            console.log(response.data);
+
+            return response.data.forEach((item) => {
+                const jurisdiction = item.jurisdiction;
+                const firstName = item.firstName;
+                const lastName = item.lastName;
+                $(supervisorList).append(
+                    `<p class='lightfeather-list-row'>${jurisdiction} - ${lastName}, ${firstName}</p>`
+                );
+            });
         })
         .fail(function (error) {
-            console.log('POST: /api/v1/account/login/ | status: error');
+            console.log('GET: /api/supervisors/ | status: error');
             console.log(`Error Received: ${error}`);
             console.dir(error);
             console.table(error);
         })
         .always(function () {
-            console.log('POST: /api/v1/account/login/ | status: completed');
+            console.log('GET: /api/supervisors/ | status: completed');
         });
 };
 
-export const logout = () => {
-    console.log('Begin capture form data.');
-    $.ajax({
-        method: 'post',
-        url: '/api/v1/account/logout/'
-    })
-        .done(function () {
-            console.log('POST: /api/v1/account/logout/ | status: success');
-            setTimeout(() => {
-                window.location.assign('/login');
-            }, 3000);
-        })
-        .fail(function (error) {
-            console.log('POST: /api/v1/account/logout/ | status: error');
-            console.log(`Error Received: ${error}`);
-            console.dir(error);
-            console.table(error);
-        })
-        .always(function () {
-            console.log('POST: /api/v1/account/logout/ | status: completed');
-        });
-};
-
-export const register = (
-    firstName,
-    lastName,
-    email,
-    password,
-    passwordConfirm
-) => {
+export const register = (firstName, lastName, email, password, passwordConfirm) => {
     console.log('Begin capture form data.');
     $.ajax({
         method: 'post',
